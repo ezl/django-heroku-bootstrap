56e6fd28bf55c219caa892fe505d9f937caef679
ezl <ericzliu@gmail.com>
2013-03-17 15:10:41 -0500

    add django extensions to dev settings apps

--- settings/dev.py
+++ settings/dev.py
@@ -4,6 +4,10 @@ from common import *
 DEBUG = TEMPLATE_DEBUG = True
 INTERNAL_IPS = ["127.0.0.1"]
 
+INSTALLED_APPS += (
+    'django_extensions',
+)
+
 STATIC_URL = '/static/'
 
 DATABASES = {'default' : {'ENGINE' : 'django.db.backends.sqlite3', 'NAME' : os.path.join(os.path.dirname(__file__),'../data.db')}}
